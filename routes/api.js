3835bfc3e061a6da0f7c65b36bd13f083b67ebf9
Theobasallaje <theobasallaje@gmail.com>
2022-08-26 02:12:31 -0500

    Cleaning up api

--- routes/api.js
+++ routes/api.js
@@ -4,8 +4,6 @@ const { log } = require("console");
 const { response } = require("express");
 const fetch = require("node-fetch");
 const path = require('path');
-// import '../utils/sections'
-// import { BOOKS } from '../utils/bookInfo';
 const bookInfo = require('../utils/bookInfo')
 
 router.get("/", (req, res) => {
@@ -14,28 +12,18 @@ router.get("/", (req, res) => {
 
 router.get("/api/bookpage/:book", async (req, res) => {
   let chapters = 0;
-  // let book = req.params.book;
   let book = req.params.book;
-  // console.log('req.params.book: ', book, typeof (book));
-  // console.log('Book: ', bookInfo.BOOKS[book].CHAPTER_COUNT);
-  // console.log('req.params: ', req.params);
   chapters = bookInfo.BOOKS[book].CHAPTER_COUNT;
-  // console.log(data);
-  // console.log(chapters);
   let promises = [];
-  // console.log(chapters);
   for (let i = 1; i < chapters; i++) {
-    // console.log({ i, chapters });
     promises.push(fetch(
       `https://api.scripture.api.bible/v1/bibles/9879dbb7cfe39e4d-04/chapters/${book}.${i}?include-verse-numbers=false`,
       {
-        // method: 'GET',
         headers: { 'api-key': process.env.API_KEY },
       }
     ).then(d => d.json()))
   }
   const result = await Promise.all(promises)
-  // console.log('RESULT: ', result);
   res.json(result);
 });
 
