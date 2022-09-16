763fb318086ae1a4cf874c9d99f555ef4b6f2f68
Kaitlyn Niesen <kaitlynniesen@annalizas-mbp.myfiosgateway.com>
2022-09-16 00:21:53 -0400

    Adding section info

---
+++ client/src/sectionInfo.js
@@ -0,0 +1,34 @@
+module.exports = {
+    SECTIONS: {
+        THE_LAW: {
+            SECTION_NAME: 'The Law (Torah',
+            QUOTE: 'Hear, O Israel: The LORD our God, the LORD is one.',
+            IMAGE:'./images/imageThe_Law.png',
+            BOOKS: ['Genesis', 'Exodus', 'Leviticus', 'Numbers', 'Deuteronomy',]
+        },
+        THE_PROPHETS: {
+            SECTION_NAME: "The Prophets (Nevi'im)",
+            QUOTE: 'He shall build a house for my name, and I will establish the throne of his kingdom forever.',
+            IMAGE: './images/imageThe_Prophet.png',
+            BOOKS: ['Joshua', 'Judges', '1 Samuel', '2 Samuel', '1 Kings', '2 Kings', 'Isaiah', 'Jeremiah', 'Ezekiel','Hosea', 'Joel', 'Amos', 'Obadiah', 'Jonah', 'Micah', 'Nahum', 'Habakkuk', 'Zephaniah', 'Haggai', 'Zechariah', 'Malachi',]
+        },
+        THE_WRITINGS: {
+            SECTION_NAME: 'The Writings (Ketuvim)',
+            QUOTE: 'Wisdom cries aloud in the street, in the markets she raises her voice',
+            IMAGE: './images/imageThe_Writings.png',
+            BOOKS: ['Psalms', 'Proverbs', 'Job', 'Song of Solomon', 'Ruth', 'Lamentations', 'Ecclesiastes', 'Esther', 'Daniel', 'Ezra', 'Nehemiah', '1 Chronicles', '2 Chronicles',]
+        },
+        THE_GOSPELS_AND_ACTS: {
+            SECTION_NAME: 'The Gospels and Acts',
+            QUOTE: 'And the Word became flesh and dwelt among us, and we have seen his glory, glory as of the only Son from the Father, full of grace and truth.',
+            IMAGE: './images/imageThe_Gospels_and_Acts',
+            BOOKS: ['Matthew', 'Mark', 'John', 'Luke', 'Acts'],
+        },
+        THE_LETTERS: {
+            SECTION_NAME: 'The Letters',
+            QUOTE: 'I do it all for the sake of the gospel, that I may share with them in its blessings.',
+            IMAGE: './images/imageThe_Letters',
+            BOOKS:['Romans', '1 Corinthians', '2 Corinthians', 'Galatians', 'Ephesians', 'Philippians', 'Colossians', '1 Thessalonians', '2 Thessalonians', '1 Timothy', '2 Timothy', 'Titus', 'Philemon', 'Hebrews', 'James', '1 Peter', '2 Peter', '1 John', '2 John', '3 John', 'Jude'],
+            },
+        },
+    }
