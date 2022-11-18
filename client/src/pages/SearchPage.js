6f4ec3850b42f599ee255302bed755ec2eaa57d1
Kaitlyn Niesen <kaitlynniesen@annalizas-mbp.myfiosgateway.com>
2022-11-18 00:38:29 -0500

    Adding conditional logic for text and results

--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "bulma/css/bulma.min.css";
 import styles from "./SearchPage.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -7,12 +7,18 @@ import { Link } from "react-router-dom";
 import Card from "../components/Card.js";
 
 function SearchPage() {
-//   const [reference, setReference] = useState("");
-//   const [text, setText] = useState("");
-//   const [bookId, setBookId] = useState("");
+  //   const [reference, setReference] = useState("");
+  //   const [text, setText] = useState("");
+  //   const [bookId, setBookId] = useState("");
   const [verses, setVerses] = useState([]);
+  const [showPreviewText, setShowPreviewText] = useState(true);
+  const [showResults, setShowResults] = useState(false);
+  const searchRef = useRef();
 
   const handleSearch = (e) => {
+    searchRef.current.blur();
+    setShowPreviewText(false);
+    setShowResults(true);
     console.log(e.target.value);
     fetch(`/api/searchpage/${e.target.value}`)
       .then((response) => {
@@ -29,16 +35,22 @@ function SearchPage() {
         console.log(err);
       });
   };
+  const handleFocus = () => {
+    setShowPreviewText(true);
+    setShowResults(false);
+  };
   return (
     <>
       <div className={`container ${styles.searchPageContainer}`}>
         <div className={`field ${styles.searchBar}`}>
           <div className="control has-icons-left has-icons-right">
             <input
+              ref={searchRef}
               className="input is-large is-rounded"
               type="text"
               placeholder="Search"
               onKeyPress={(e) => e.key === "Enter" && handleSearch(e)}
+              onFocus={handleFocus}
             />
             <span className="icon is-medium is-left">
               <Link to="/" className={styles.anchorClass}>
@@ -52,29 +64,32 @@ function SearchPage() {
               <i className="fa fa-times"></i>
             </span>
           </div>
-          <p className={`is-size-5 ${styles.previewText}`}>
-            <br />
-            <br />
-            &nbsp; &nbsp; Search for passages or phrases.
-            <br />
-            <br />
-            &nbsp; &nbsp; Ex: John 1:1; Garden
-          </p>
+          {showPreviewText && (
+            <p className={`is-size-5 ${styles.previewText}`}>
+              <br />
+              <br />
+              &nbsp; &nbsp; Search for passages or phrases.
+              <br />
+              <br />
+              &nbsp; &nbsp; Ex: John 1:1; Garden
+            </p>
+          )}
         </div>
-        <div className={`${styles.resultsContainer}`}>
-          <h4 className={`title is-4`}>Books</h4>
-          <h4 className={`title is-4 ${styles.results}`}>Passages</h4>
-        
-        {
-          verses.map((verse, index) => (
-            <Card
-              key={index}
-              reference={verse.reference}
-              text={verse.text}
-              bookId={verse.bookId}
-            />
-          ))}
+        {showResults && (
+          <div className={`${styles.resultsContainer}`}>
+            <h4 className={`title is-4`}>Books</h4>
+            <h4 className={`title is-4 ${styles.results}`}>Passages</h4>
+
+            {verses.map((verse, index) => (
+              <Card
+                key={index}
+                reference={verse.reference}
+                text={verse.text}
+                bookId={verse.bookId}
+              />
+            ))}
           </div>
+        )}
       </div>
     </>
   );
