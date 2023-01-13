85e044ae4bb29c160d333925a16403392e1a3399
Kaitlyn Niesen <kaitlynniesen@annalizas-mbp.myfiosgateway.com>
2023-01-13 00:14:20 -0500

    Fixing back button

--- client/src/pages/BookPage.js
+++ client/src/pages/BookPage.js
@@ -1,40 +1,46 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "bulma/css/bulma.min.css";
 import styles from "./BookPage.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import parse from "html-react-parser";
 
 function BookPage() {
   let params = useParams();
+  let navigate = useNavigate();
+  const handleBack = () => navigate(-1);
   const [content, setContent] = useState("retrieving content...");
   const [bookName, setBookName] = useState("");
   const [nextBook, setNextBook] = useState("");
   const [nextApiName, setNextApiName] = useState("");
+  const shouldMount = useRef(true);
 
   useEffect(() => {
-    console.log("params: ", params.book);
-    fetch(`/api/bookPage/${params.book}`)
-      .then((response) => {
-        return response.json(); // parses response to only give use the data we want
-      })
-      .then((data) => {
-        // console.log(data);
-        let content = "";
-        setBookName(data.bookName);
-        setNextBook(data.nextBook);
-        setNextApiName(data.nextApiName);
-        console.log(data);
-        data.result.forEach((book) => {
-          content += book.data.content;
+    if (shouldMount.current) {
+      shouldMount.current = false;
+      console.log("params: ", params.book);
+      fetch(`/api/bookPage/${params.book}`)
+        .then((response) => {
+          return response.json(); // parses response to only give use the data we want
+        })
+        .then((data) => {
+          // console.log(data);
+          let content = "";
+          setBookName(data.bookName);
+          setNextBook(data.nextBook);
+          setNextApiName(data.nextApiName);
+          console.log(data);
+          data.result.forEach((book) => {
+            content += book.data.content;
+          });
+          // console.log('content: ', content);
+          setContent(content);
+        })
+        .catch((err) => {
+          console.log(err);
         });
-        // console.log('content: ', content);
-        setContent(content);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    }
   });
 
   return (
@@ -45,10 +51,15 @@ function BookPage() {
         aria-label="main navigation"
       >
         <div className={`navbar-brand ${styles.navbarBrandContainer}`}>
-          <span className="icon is-medium">
-            <FontAwesomeIcon className={styles.arrowIcon} icon={faArrowLeft} />
-          </span>
-          <div className="subtitle is-4">Back</div>
+          <div onClick={handleBack} className={styles.backContainer}>
+            <span className="icon is-medium">
+              <FontAwesomeIcon
+                className={styles.arrowIcon}
+                icon={faArrowLeft}
+              />
+            </span>
+            <div className="subtitle is-4">Back</div>
+          </div>
         </div>
       </nav>
       <div className={styles.bookPageHeaderContainer}>
