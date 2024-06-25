a003a623c567eec72c751cb8b5737aa3a01feec1
Theobasallaje <theobasallaje@gmail.com>
2024-06-25 18:45:20 -0500

    Fixing sizing and spacing

--- client/src/pages/SectionPage.jsx
+++ client/src/pages/SectionPage.jsx
@@ -1,18 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import "bulma/css/bulma.min.css";
-import { BookSectionMap } from "./BookSectionMap";
 import styles from "./SectionPage.module.scss";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
-import { Link, useLocation, useParams } from "react-router-dom";
-import Header from "../components/Header";
+import { Link, useLocation } from "react-router-dom";
 import { useCurrentSection } from "../utils/Hooks";
 
 const SectionPage = () => {
     const location = useLocation();
     const currentSection = useCurrentSection();
-    const image = "/images/" + currentSection.image + ".png";
-
     const url = location.pathname;
 
     useEffect(() => {
@@ -24,12 +18,11 @@ const SectionPage = () => {
     return (
         <div>
             <div className={styles.sectionPageContainer}>
-                <div className={`container ${styles.sectionContainer}`}>
-                    <h2 className={`title is-2 ${styles.sectionHeader}`}>{currentSection.title}</h2>
+                <div className={`${styles.sectionContainer}`}>
+                    <h2 className={`${styles.sectionHeader}`}>{currentSection.title}</h2>
                     <div className={styles.quotedTextContainer}>
                         <p className={styles.quotedText}>{currentSection.quote}</p>
                     </div>
-
                     <div className={styles.buttonContainer}>
                         {currentSection.books.map((book, index) => (
                             <Link key={index} to={book.route} className={styles.sectionPageLink}>
