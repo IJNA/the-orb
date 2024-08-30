ccf0be97e784479f0017febf965d31139a04b696
Theobasallaje <theobasallaje@gmail.com>
2024-08-29 22:48:01 -0500

    Editing text styles for about page

--- client/src/utils/Hooks.jsx
+++ client/src/utils/Hooks.jsx
@@ -1,5 +1,5 @@
 import { useLocation } from "react-router-dom";
-import { BookSectionMap } from "../pages/BookSectionMap";
+import { BookSectionMap } from "../pages/BookSectionMap.jsx";
 
 
 export const useCurrentSection = () => {
